fe698e7f86afe15dce78d305d02c1b803bcf026e
PatrickCooke <cookepa1@gmail.com>
2016-04-12 16:10:48 -0400

    all operators are working

--- Calc/ViewController.m
+++ Calc/ViewController.m
@@ -18,44 +18,138 @@
 
 #pragma mark - Global Variables
 
-int leftSideNumber = 0;
-int rightSideNumber = 0;
+float leftSideNumber = 0;
+float rightSideNumber = 0;
 bool operatorPressed = false;
+bool add = false;
+bool sub = false;
+bool multi = false;
+bool divide = false;
+float result = 0;
 
-#pragma mark - buttons
+
+#pragma mark - Operator Buttons
 
 - (IBAction)clearButtonPressed:(id)button {
     NSLog(@"Clear Pressed");
-    _currentNumberlabel.text = [NSString stringWithFormat:@"clear"];
+    _currentNumberlabel.text = [NSString stringWithFormat:@""];
 }
 
 - (IBAction)equalsButtonPressed:(id)button {
     NSLog(@"Equal Pressed");
-    int result = leftSideNumber+rightSideNumber;
-    _currentNumberlabel.text = [NSString stringWithFormat:@"%i",result];
+    
+    if (add) {
+        result = leftSideNumber+rightSideNumber;
+    } else if (sub){
+        result = leftSideNumber-rightSideNumber;
+    } else if (multi) {
+        result =leftSideNumber*rightSideNumber;
+    } else if (divide){
+        result = leftSideNumber/rightSideNumber;
+    }
+    //int result = leftSideNumber+rightSideNumber;
+    _currentNumberlabel.text = [NSString stringWithFormat:@"%0f",result];
 }
 
-- (IBAction)oneButtonPressed:(id)button {
-    NSLog(@"One Pressed");
-    _currentNumberlabel.text = @"1";
-    if (operatorPressed) {
-        rightSideNumber = 1;
-    } else {
-        leftSideNumber = 1;
-    }
-}
 
 - (IBAction)plusButtonPressed:(id)button {
     NSLog(@"Plus Pressed");
     operatorPressed = true;
+    add = true;
+    sub = false;
+    multi = false;
+    divide = false;
+    _currentNumberlabel.text = @"";
 }
 
+- (IBAction)subButtonPressed:(id)button {
+    NSLog(@"sub Pressed");
+    operatorPressed = true;
+    add = false;
+    sub = true;
+    multi = false;
+    divide = false;
+    _currentNumberlabel.text = @"";
+}
 
+- (IBAction)multiButtonPressed:(id)button {
+    NSLog(@"multi Pressed");
+    operatorPressed = true;
+    add = false;
+    sub = false;
+    multi = true;
+    divide = false;
+    _currentNumberlabel.text = @"";
+}
 
+- (IBAction)divideButtonPressed:(id)button {
+    NSLog(@"divide Pressed");
+    operatorPressed = true;
+    add = false;
+    sub = false;
+    multi = false;
+    divide = true;
+    _currentNumberlabel.text = @"";
+}
 
+# pragma mark - Number Buttons
 
+- (IBAction)oneButtonPressed:(id)button {
+    NSLog(@"One Pressed");
+    _currentNumberlabel.text = [_currentNumberlabel.text stringByAppendingString:@"1"];
+    if (operatorPressed) {
+        rightSideNumber = [_currentNumberlabel.text floatValue];
+    } else {
+        leftSideNumber = [_currentNumberlabel.text floatValue];
+    }
+}
 
+- (IBAction)twoButtonPressed:(id)button {
+    NSLog(@"One Pressed");
+    _currentNumberlabel.text = [_currentNumberlabel.text stringByAppendingString:@"2"];
+    if (operatorPressed) {
+        rightSideNumber = [_currentNumberlabel.text floatValue];
+    } else {
+        leftSideNumber = [_currentNumberlabel.text floatValue];
+    }
+}
 
+- (IBAction)threeButtonPressed:(id)button {
+    NSLog(@"One Pressed");
+    _currentNumberlabel.text = [_currentNumberlabel.text stringByAppendingString:@"3"];
+    if (operatorPressed) {
+        rightSideNumber = [_currentNumberlabel.text floatValue];
+    } else {
+        leftSideNumber = [_currentNumberlabel.text floatValue];
+    }
+}
+- (IBAction)fourButtonPressed:(id)button {
+    NSLog(@"One Pressed");
+    _currentNumberlabel.text = [_currentNumberlabel.text stringByAppendingString:@"4"];
+    if (operatorPressed) {
+        rightSideNumber = [_currentNumberlabel.text floatValue];
+    } else {
+        leftSideNumber = [_currentNumberlabel.text floatValue];
+    }
+}
+- (IBAction)fiveButtonPressed:(id)button {
+    NSLog(@"One Pressed");
+    _currentNumberlabel.text = [_currentNumberlabel.text stringByAppendingString:@"5"];
+    if (operatorPressed) {
+        rightSideNumber = [_currentNumberlabel.text floatValue];
+    } else {
+        leftSideNumber = [_currentNumberlabel.text floatValue];
+    }
+}
+- (IBAction)sixButtonPressed:(id)button {
+    NSLog(@"One Pressed");
+    _currentNumberlabel.text = [_currentNumberlabel.text stringByAppendingString:@"6"];
+    if (operatorPressed) {
+        rightSideNumber = [_currentNumberlabel.text floatValue];
+    } else {
+        leftSideNumber = [_currentNumberlabel.text floatValue];
+    }
+}
 
 
 #pragma mark - life cycle stages
