4a414378b4b2ea36204ce0b98b2117e0e5e7a5b6
PatrickCooke <cookepa1@gmail.com>
2016-04-12 15:07:47 -0400

    1 + 1 = 2 works

--- Calc/ViewController.m
+++ Calc/ViewController.m
@@ -10,10 +10,55 @@
 
 @interface ViewController ()
 
+@property(nonatomic, weak) IBOutlet UILabel     *currentNumberlabel;
+
 @end
 
 @implementation ViewController
 
+#pragma mark - Global Variables
+
+int leftSideNumber = 0;
+int rightSideNumber = 0;
+bool operatorPressed = false;
+
+#pragma mark - buttons
+
+- (IBAction)clearButtonPressed:(id)button {
+    NSLog(@"Clear Pressed");
+    _currentNumberlabel.text = [NSString stringWithFormat:@"clear"];
+}
+
+- (IBAction)equalsButtonPressed:(id)button {
+    NSLog(@"Equal Pressed");
+    int result = leftSideNumber+rightSideNumber;
+    _currentNumberlabel.text = [NSString stringWithFormat:@"%i",result];
+}
+
+- (IBAction)oneButtonPressed:(id)button {
+    NSLog(@"One Pressed");
+    _currentNumberlabel.text = @"1";
+    if (operatorPressed) {
+        rightSideNumber = 1;
+    } else {
+        leftSideNumber = 1;
+    }
+}
+
+- (IBAction)plusButtonPressed:(id)button {
+    NSLog(@"Plus Pressed");
+    operatorPressed = true;
+}
+
+
+
+
+
+
+
+
+
+#pragma mark - life cycle stages
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
